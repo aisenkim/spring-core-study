595672c7e9c6ec60fb26fcc1fa713e09e9c03653
aisen <aisenkim15@gmail.com>
2021-07-09 10:54:13 +0900

    Added appconfig file to manage the whole application with injecting to bring in DI, now following the SOLID principle

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,26 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+/**
+ * [Dependency Injection]
+ * Set values through interface, injecting it to services
+ * so that it can follow the SOLID principle DIP and OCP
+ * This allows the service class to just focus on the service it self
+ */
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+}
