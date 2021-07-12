bf3f4e3e72a6ca226d0b1e2ef860e976a5f53d5e
Aisen Kim <aisenkim15@gmail.com>
2021-07-13 01:56:43 +0900

    added spring container and bean. Don't create AppConfig ourselves but now the container manages all

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,6 +8,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * [Dependency Injection]
@@ -15,20 +17,25 @@ import hello.core.order.OrderServiceImpl;
  * so that it can follow the SOLID principle DIP and OCP
  * This allows the service class to just focus on the service it self
  */
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy() );
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
